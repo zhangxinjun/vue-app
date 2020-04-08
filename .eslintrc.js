fcccd3ff8a2e7308a310a9141a18de2e3165ffdd
zhangxinjun <759561217@qq.com>
2020-04-08 22:41:49 +0800

    修改了eslint的相关配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true, // 支持node语法
+    es6: true // 支持es6语法
   },
   'extends': [
     'plugin:vue/essential',
@@ -12,6 +13,19 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "spaced-comment": [ // 注释后预留空格
+      "error",
+      "always"
+    ],
+    "space-before-function-paren": [ // 括号与函数名加空格
+      "error", "always"
+    ],
+    "no-multiple-empty-lines": [ // 最大空行数
+      "error", {
+        "max": 2
+      }
+    ],
+    "comma-dangle": [2, 'never'], // 对象最后一个key不允许带，
   }
 }
