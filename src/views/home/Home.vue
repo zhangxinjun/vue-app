4484f842912496a14407eab929a91ef47c2de1bb
zhangxinjun <759561217@qq.com>
2020-04-18 21:49:22 +0800

    封装navbar组件并在首页引入

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-      <h2>这是home组件</h2>
+     <NavBar class="navbar">
+       <div slot="center">这是home组件的导航</div>
+     </NavBar>
   </div>
 </template>
 
 <script>
+import NavBar from "common/common/navbar/NavBar"
 export default {
-
+  components:{
+    NavBar
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .navbar{
+    color: #fff;
+    background: #ff0088
+  }
 </style>
